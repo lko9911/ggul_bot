1a748a1a7c2045b4cfb2cc305b74c46dd236d1ad
lko9911 <160494158+lko9911@users.noreply.github.com>
2025-04-27 13:42:57 +0900

    Add files via upload

---
+++ strawberry_publisher_pkg/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+package_name = 'strawberry_publisher_pkg'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=find_packages(),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=[
+        'setuptools',
+        'opencv-python',
+        'ultralytics',
+        'tensorflow',
+    ],
+    zip_safe=True,
+    maintainer='lko',
+    maintainer_email='lko991111@gmail.com',
+    description='딸기 위치 퍼블리셔',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'strawberry_publisher = strawberry_publisher_pkg.strawberry_publisher:main',
+        ],
+    },
+)
