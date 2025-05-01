2174d8172e87eb5560df18a62b176485452f156c
lko9911 <160494158+lko9911@users.noreply.github.com>
2025-05-01 14:46:08 +0900

    Delete ggul_bot_v6_config directory

--- ggul_bot_v6_config/launch/moveit_rviz.launch.py
+++
@@ -1,7 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_moveit_rviz_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("ggul_bot_v6", package_name="ggul_bot_v6_config").to_moveit_configs()
-    return generate_moveit_rviz_launch(moveit_config)
--- ggul_bot_v6_config/launch/static_virtual_joint_tfs.launch.py
+++
@@ -1,7 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_static_virtual_joint_tfs_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("ggul_bot_v6", package_name="ggul_bot_v6_config").to_moveit_configs()
-    return generate_static_virtual_joint_tfs_launch(moveit_config)
