df264d514df594b26aae64685ec661eb39ddf524
lko9911 <160494158+lko9911@users.noreply.github.com>
2025-05-03 21:54:07 +0900

    Update ik_ws_publisher.py

--- websocket_pkg/websocket_pkg/ik_ws_publisher.py
+++ websocket_pkg/websocket_pkg/ik_ws_publisher.py
@@ -1,6 +1,6 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float64MultiArray
+from sensor_msgs.msg import JointState
 import asyncio
 import websockets
 import json
@@ -9,21 +9,21 @@ class IKWebSocketPublisher(Node):
     def __init__(self):
         super().__init__('ik_ws_publisher')
         self.subscription = self.create_subscription(
-            Float64MultiArray,
-            '/ik_solution',
+            JointState,  # JointState 메시지로 변경
+            '/joint_states',  # 퍼블리시된 조인트 상태 토픽
             self.listener_callback,
             10)
         self.subscription  # prevent unused variable warning
 
     def listener_callback(self, msg):
-        joint_values = msg.data
-        self.get_logger().info(f'Received IK Solution: {joint_values}')
+        joint_values = msg.position  # JointState의 position 필드에서 조인트 값 추출
+        self.get_logger().info(f'Received Joint States: {joint_values}')
 
         # 웹소켓으로 전송
         asyncio.run(self.send_to_websocket(joint_values))
 
     async def send_to_websocket(self, joint_values):
-        uri = "ws://192.168.150.149:8765"  
+        uri = "ws://192.168.150.149:8765"  # 웹소켓 서버 주소
         data = {
             "joint_values": joint_values
         }
