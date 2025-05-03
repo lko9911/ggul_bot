9025ec476c3f19055713c23bcdb29038685c4696
lko9911 <160494158+lko9911@users.noreply.github.com>
2025-05-03 22:07:28 +0900

    Update ik_subscriber_node.cpp

--- ik_solver_cpp/src/ik_subscriber_node.cpp
+++ ik_solver_cpp/src/ik_subscriber_node.cpp
@@ -17,7 +17,7 @@ public:
         robot_model_ = robot_model_loader_->getModel();
         robot_state_ = std::make_shared<moveit::core::RobotState>(robot_model_);
 
-        planning_group_ = "manipulator2";  
+        planning_group_ = "manipulator7";  
         joint_model_group_ = robot_model_->getJointModelGroup(planning_group_);
 
         // 퍼블리셔 추가
