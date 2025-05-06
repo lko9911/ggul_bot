aab5b960ce610af2150c8bd411fa1998b05e59d4
lko9911 <160494158+lko9911@users.noreply.github.com>
2025-05-06 10:12:44 +0900

    Update ik_ws_publisher.py

--- websocket_pkg/websocket_pkg/ik_ws_publisher.py
+++ websocket_pkg/websocket_pkg/ik_ws_publisher.py
@@ -22,7 +22,7 @@ class IKWebSocketPublisher(Node):
         asyncio.run(self.send_to_websocket(joint_values))
 
     async def send_to_websocket(self, joint_values):
-        uri = "ws://<IPv4>:8765"
+        uri = "ws://192.168.123.103:8766"
         data = {
             "joint_values": joint_values
         }
